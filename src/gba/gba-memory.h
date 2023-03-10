dc1197253b04ec91e8e190f746527965f808bd2f
xiang.weng <xiang.weng@anlogic.com>
2023-03-10 20:20:44 +0800

    modify files

--- src/gba/gba-memory.h
+++ src/gba/gba-memory.h
@@ -14,6 +14,38 @@
 #include "gba-gpio.h"
 #include "gba-savedata.h"
 
+/*
+GBA Memory Map
+
+General Internal Memory
+  00000000-00003FFF   BIOS - System ROM         (16 KBytes)
+  00004000-01FFFFFF   Not used
+  02000000-0203FFFF   WRAM - On-board Work RAM  (256 KBytes) 2 Wait
+  02040000-02FFFFFF   Not used
+  03000000-03007FFF   WRAM - On-chip Work RAM   (32 KBytes)
+  03008000-03FFFFFF   Not used
+  04000000-040003FE   I/O Registers
+  04000400-04FFFFFF   Not used
+
+Internal Display Memory
+  05000000-050003FF   BG/OBJ Palette RAM        (1 Kbyte)
+  05000400-05FFFFFF   Not used
+  06000000-06017FFF   VRAM - Video RAM          (96 KBytes)
+  06018000-06FFFFFF   Not used
+  07000000-070003FF   OAM - OBJ Attributes      (1 Kbyte)
+  07000400-07FFFFFF   Not used
+
+External Memory (Game Pak)
+  08000000-09FFFFFF   Game Pak ROM/FlashROM (max 32MB) - Wait State 0
+  0A000000-0BFFFFFF   Game Pak ROM/FlashROM (max 32MB) - Wait State 1
+  0C000000-0DFFFFFF   Game Pak ROM/FlashROM (max 32MB) - Wait State 2
+  0E000000-0E00FFFF   Game Pak SRAM    (max 64 KBytes) - 8bit Bus width
+  0E010000-0FFFFFFF   Not used
+
+Unused Memory Area
+  10000000-FFFFFFFF   Not used (upper 4bits of address bus unused)
+*/
+
 enum GBAMemoryRegion {
 	REGION_BIOS = 0x0,
 	REGION_WORKING_RAM = 0x2,
@@ -110,10 +142,10 @@ struct GBADMA {
 };
 
 struct GBAMemory {
-	uint32_t* bios;
-	uint32_t* wram;
-	uint32_t* iwram;
-	uint32_t* rom;
+	uint32_t* bios;	//0x00000000-0x00003FFF   BIOS - System ROM (16 KBytes)
+	uint32_t* wram;	//0x02000000-0x0203FFFF   WRAM - On-board Work RAM (256 KBytes) 2 Wait
+	uint32_t* iwram;//0x03000000-0x03007FFF   WRAM - On-chip Work RAM (32 KBytes)
+	uint32_t* rom;	//0x08000000-0x0FFFFFFF   External Memory (Game Pak)
 	uint16_t io[SIZE_IO >> 1];
 
 	struct GBACartridgeGPIO gpio;
@@ -133,7 +165,7 @@ struct GBAMemory {
 	int activeRegion;
 	uint32_t biosPrefetch;
 
-	struct GBADMA dma[4];
+	struct GBADMA dma[4];	//The GBA includes four DMA channels，GBA包含4个DMA通道
 	int activeDMA;
 	int32_t nextDMA;
 	int32_t eventDiff;
